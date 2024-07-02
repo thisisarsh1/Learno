a67077c5e45729b5211e53bae7b60c608a93849b
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-07-02 12:57:56 +0530

    vercel try

--- learno/src/app/(router)/Dashboard/page.jsx
+++ learno/src/app/(router)/Dashboard/page.jsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { use, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import WelcomeBanner from './components/welcomeBanner'
 import { useUser } from "@clerk/nextjs";
 import Sidebanners from '../courses/_components/Sidebanners';
