53519d41fec8844eef5cab3a3573134310526e66
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-07-04 01:47:09 +0530

    vercel final 1

--- learno/src/app/(router)/courses/page.jsx
+++ learno/src/app/(router)/courses/page.jsx
@@ -22,7 +22,10 @@ function Page() {
     <div>
       {isMobile ? (
         <div className="text-center mt-5">
-          <p>Please view this page on a laptop or desktop.</p>
+          <div className="text align-middle">
+        <div className='  text-4xl font-semibold text-gray-700'> Please view this website on PC or laptop </div>
+        <div className='   text-2xl font-semibold mt-5 text-gray-500'> it is  not designed for mobiles <span className='text-green-600'>,YET</span> </div>
+      </div>
         </div>
       ) : (
         <div className='grid grid-cols-4 mt-3'>
