43be6710e243907013b2aa4298fdad09f0691b00
Rehbar khan <rehbarkhan.2711@gmail.com>
2024-07-04 01:41:21 +0530

    vercel final

--- learno/src/app/(router)/Dashboard/page.jsx
+++ learno/src/app/(router)/Dashboard/page.jsx
@@ -1,41 +1,58 @@
 "use client"
-import React, { useEffect, useState } from 'react'
-import WelcomeBanner from './components/WelcomeBanner'
+import React, { useEffect, useState } from 'react';
+import WelcomeBanner from './components/WelcomeBanner';
 import { useUser } from "@clerk/nextjs";
 import Sidebanners from '../courses/_components/Sidebanners';
-import InProgress from './components/InProgress'; 
+import InProgress from './components/InProgress';
 import GlobalApi from '@/app/utils/GlobalApi';
-function page() {
+
+function Page() {
   const { user } = useUser();
- 
-  const [userEnrolledCourse,setUserEnrolledCourse]=useState([])
-useEffect(()=>{
-  user&&GetUserProgressDeets();
+  const [userEnrolledCourse, setUserEnrolledCourse] = useState([]);
+  const [isMobile, setIsMobile] = useState(false);
 
-},[user])
-  const GetUserProgressDeets=()=>{
-    GlobalApi.GetProgressList(user?.primaryEmailAddress?.emailAddress).then((resp)=>{
-      setUserEnrolledCourse(resp.userEnrollcourses)
+  useEffect(() => {
+    user && GetUserProgressDeets();
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, [user]);
+
+  const GetUserProgressDeets = () => {
+    GlobalApi.GetProgressList(user?.primaryEmailAddress?.emailAddress).then((resp) => {
+      setUserEnrolledCourse(resp.userEnrollcourses);
       // console.log(resp.userEnrollcourses)
-    })
-  }
-  return (
-    <div className='grid grid-cols-4 mt-3'>
-    <div className=' col-span-3  ml-1 '>
-    
-      <WelcomeBanner user={user}></WelcomeBanner>
-      <InProgress userEnrolledCourse={userEnrolledCourse}></InProgress>
-     
-    </div>
-    <div className=' col-span-1 '>
-      <Sidebanners/>
-    </div>
+    });
+  };
 
-    
-    
-  </div>
-   
-  )
+  return (
+    <div>
+      {isMobile ? (
+        <div className="text-center mt-5">
+         <div className="text align-middle">
+        <div className='  text-4xl font-semibold text-gray-700'> Please view this website on PC or laptop </div>
+        <div className='   text-2xl font-semibold mt-5 text-gray-500'> it is  not designed for mobiles <span className='text-green-600'>,YET</span> </div>
+      </div>
+        </div>
+      ) : (
+        <div className='grid grid-cols-4 mt-3'>
+          <div className='col-span-3 ml-1'>
+            <WelcomeBanner user={user} />
+            <InProgress userEnrolledCourse={userEnrolledCourse} />
+          </div>
+          <div className='col-span-1'>
+            <Sidebanners />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default page
+export default Page;
--- learno/src/app/(router)/courses/page.jsx
+++ learno/src/app/(router)/courses/page.jsx
@@ -7,6 +7,15 @@ import Sidebanners from './_components/Sidebanners'
 
 function page() {
   return (
+<div>
+    {isMobile ? (
+      <div className="text-center mt-5">
+       <div className="text align-middle">
+      <div className='  text-4xl font-semibold text-gray-700'> Please view this website on PC or laptop </div>
+      <div className='   text-2xl font-semibold mt-5 text-gray-500'> it is  not designed for mobiles <span className='text-green-600'>,YET</span> </div>
+    </div>
+      </div>
+    ) : (
     <div className='grid grid-cols-4 mt-3'>
       <div className=' col-span-3  ml-1 '>
         <Banner></Banner>
@@ -15,10 +24,10 @@ function page() {
       <div className=' col-span-1 '>
         <Sidebanners/>
       </div>
-
+  
       
       
-    </div>
+    </div>)}</div>
   )
 }
 
